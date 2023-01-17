bb67ff1c7181afc336a524d393de6f400d229638
Ovensif <imamtaufikt.ti.poliwangi@gmail.com>
2023-01-17 11:11:00 +0700

    Fix bug for unregistered document, show loading animation while sending a message @Chat App

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -2,13 +2,24 @@ import ChatRoom from "../../components/chatRoom";
 import { auth } from "../../firebase.config";
 import { useAuthState } from "react-firebase-hooks/auth";
 import LoadingSpinner from "../../components/loadingSpinner";
+import { useDocument} from "react-firebase-hooks/firestore";
+import { firebaseDB } from "../../firebase.config";
+import { doc } from "firebase/firestore";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Chat() {
+export default function Chat(req, res) {
 
   const [user, loading] = useAuthState(auth);
-  const router = useRouter()
+  const router = useRouter();
+  const [snapshotCurrentDocument, snapshotCurrentDocumentLoading] = useDocument(
+    doc(firebaseDB, "chat", `${router.query?.id}`)
+  );
+  
+  // Checking if document exist or not!
+  !snapshotCurrentDocument?.exists() && !snapshotCurrentDocumentLoading && (router.push('/chat/NQ2prTGrDb50XoQizOrp'))
 
+  // Check If User Not Authenticated!
   if(!user && !loading){
     router.push('/')
   }
--- utils/library.js
+++ utils/library.js
@@ -1,6 +1,6 @@
 const showOppositeEmail = (props) => {
 
-    let emailCurrentUser = props?.currentUser.email
+    let emailCurrentUser = props?.currentUser?.email
     let emailList = props?.owner?.owner
 
     return emailCurrentUser == emailList[0] ? emailList[1] : emailList[0];
